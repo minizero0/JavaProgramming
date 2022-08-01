2a3848d4052fedc216876adb7289ef3016f4adf8
yeongmin <dudals1069@gmail.com>
2022-08-01 11:21:49 +0900

    BJ

---
+++ src/boostcamp/CircleArea.java
@@ -0,0 +1,16 @@
+package boostcamp;
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		double PI = Math.PI;
+		System.out.println(PI);
+		
+		double radius = 10.0;
+		double CircleArea = radius * radius * PI;
+		
+		System.out.println(CircleArea);
+
+	}
+
+}
