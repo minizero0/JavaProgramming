4a4f729252bf64e63f1485441485e95b0ee32da8
yeongmin <dudals1069@gmail.com>
2022-08-01 10:14:00 +0900

    BJ

--- src/boostcamp/Letter.java
+++ src/boostcamp/Letter.java
@@ -4,7 +4,8 @@ public class Letter {
 
 	public static void main(String[] args) {
 		String name = "leezche";
-		System.out.println("hello " + name + "..." + name);
+		String myNmae = "yeongmin";
+		System.out.println("hello " + name + "..." + myName);
 
 	}
 
