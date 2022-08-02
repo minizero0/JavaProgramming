ed56ca82d01c491b3c766b1e05be69280e3c2642
yeongmin <dudals1069@gmail.com>
2022-08-02 15:18:38 +0900

    BJ

---
+++ src/boostcamp/Chap3_pro8.java
@@ -0,0 +1,27 @@
+package boostcamp;
+import java.util.Scanner;
+
+public class Chap3_pro8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수 몇개?");
+		int number = sc.nextInt();
+		
+		int arr[] = new int[number];
+		for (int i = 0; i < number; i++) {
+			arr[i] = (int)(Math.random()*100 + 1);
+			for (int j = 0; j < i; j++) {
+				if (arr[i] == arr[j])
+					i--;
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			if ((i) % 10 == 0)	
+				System.out.println();
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+}
