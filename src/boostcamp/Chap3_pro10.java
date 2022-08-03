a405216333bdc1c55ef3b02e25520e67eecb7e05
yeongmin <dudals1069@gmail.com>
2022-08-03 14:39:12 +0900

    BJ

--- src/boostcamp/Chap3_pro10.java
+++ src/boostcamp/Chap3_pro10.java
@@ -3,30 +3,28 @@ package boostcamp;
 public class Chap3_pro10 {
 
 	public static void main(String[] args) {
-		int arr[][] = new int[4][4];
+		
+		int arr[][] = new int [4][4];
 		int count = 0;
 		
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++) {
+			for (int j = 0; j < arr.length; j++) 
 				arr[i][j] = (int)(Math.random()*10 + 1);
-			}
 		}
 		
-		while(count < 6) {
+		while (count < 6) {
 			int row = (int)(Math.random()*4);
 			int col = (int)(Math.random()*4);
-	            
+			
 			if (arr[row][col] != 0) {
 				arr[row][col] = 0;
 				count++;
 			}
-			
-			else 
-				continue;
 		}
+		
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++)
-				System.out.print(arr[i][j] + " ");
+			for (int j = 0; j < arr.length; j++) 
+				System.out.printf("%3d", arr[i][j]);
 			System.out.println();
 		}
 
