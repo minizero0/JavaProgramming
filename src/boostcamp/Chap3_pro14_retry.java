d7ac9c9d23067c9c478d114fb206a2110fe11ac8
yeongmin <dudals1069@gmail.com>
2022-08-03 23:55:04 +0900

    BJ

--- src/boostcamp/Chap3_pro14_retry.java
+++ src/boostcamp/Chap3_pro14_retry.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 public class Chap3_pro14_retry {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		String course [] = {"Java", "C++", "HTMLS", "컴퓨터구조", "안드로이드"};
+		int score [] = {95, 88, 76, 62, 55};
 
 	}
 
