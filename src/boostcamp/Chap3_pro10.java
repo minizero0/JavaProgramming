10dd3ebbe3ba7e9778623decbdc5a5102a45bad8
yeongmin <dudals1069@gmail.com>
2022-08-03 14:32:59 +0900

    BJ

--- src/boostcamp/Chap3_pro10.java
+++ src/boostcamp/Chap3_pro10.java
@@ -4,6 +4,7 @@ public class Chap3_pro10 {
 
 	public static void main(String[] args) {
 		int arr[][] = new int[4][4];
+		int count = 0;
 		
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
@@ -11,6 +12,18 @@ public class Chap3_pro10 {
 			}
 		}
 		
+		while(count < 6) {
+			int row = (int)(Math.random()*4);
+			int col = (int)(Math.random()*4);
+	            
+			if (arr[row][col] != 0) {
+				arr[row][col] = 0;
+				count++;
+			}
+			
+			else 
+				continue;
+		}
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++)
 				System.out.print(arr[i][j] + " ");
