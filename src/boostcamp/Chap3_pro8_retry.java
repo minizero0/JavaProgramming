8494b898edcf5ca256a5b6746b688579b027da45
yeongmin <dudals1069@gmail.com>
2022-08-06 20:32:38 +0900

    BJ

--- src/boostcamp/Chap3_pro8_retry.java
+++ src/boostcamp/Chap3_pro8_retry.java
@@ -6,20 +6,21 @@ public class Chap3_pro8_retry {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("정수 몇개?");
+		System.out.print("정수 몇개>>");
 		int number = sc.nextInt();
-		int array [] = new int[number];
+		int arr[] = new int[number];
 		
 		for (int i = 0; i < number; i++) {
-			array[i] = (int)(Math.random()*100 + 1);
+			arr[i] = (int)(Math.random()*100 + 1);
 			for (int j = 0; j < i; j++) {
-				if (array[i] == array[j])
+				if (arr[i] == arr[j])
 					i--;
 			}
 		}
-		for (int i = 0; i < array.length; i++) {
-			System.out.print(array[i] + " ");
-			if ((i + 1) % 10 == 0)
+		
+		for (int i = 0; i < number; i++) {
+			System.out.print(arr[i] + " ");
+			if ((i + 1) % 10 == 0)	
 				System.out.println();
 		}
 		
