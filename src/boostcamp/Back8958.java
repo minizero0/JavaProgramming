ab46c71d181b056b031d54f194a06e8e932b8989
yeongmin <dudals1069@gmail.com>
2022-08-08 16:43:53 +0900

    BJ

--- src/boostcamp/Back8958.java
+++ src/boostcamp/Back8958.java
@@ -5,17 +5,20 @@ public class Back8958 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		char score[] = new char[80];
+		String score;
 		
 		int number = sc.nextInt();
 		for (int i = 0; i < number; i++) {
-			score = sc.next();
 			int sum = 0;
 			int count = 1;
+			score = sc.next();
 			for (int j = 0; j < score.length(); j++) {
-				if (score[j] == 'O')
+				if (score.charAt(j) == 'O')
 					sum += count++;
+				else if(score.charAt(j) == 'X')
+					count = 1;
 			}
+			System.out.println(sum);
 		}
 
 	}
