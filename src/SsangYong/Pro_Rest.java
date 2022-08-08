31f45ead92a4ebb5f2e3a0dd800e2db03a12660d
yeongmin <dudals1069@gmail.com>
2022-08-08 16:19:20 +0900

    BJ

---
+++ src/SsangYong/Pro_Rest.java
@@ -0,0 +1,19 @@
+package SsangYong;
+import java.util.Scanner;
+
+public class Pro_Rest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수를 입력하세요");
+		int n = sc.nextInt();
+		
+		if (n % 2 == 0)
+			System.out.println("짝수입니다.");
+		else
+			System.out.println("홀수입니다.");
+
+	}
+
+}
