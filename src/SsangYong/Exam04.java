b2e3f3610a671455ab243a9ba1111a24f74afaeb
yeongmin <dudals1069@gmail.com>
2022-08-09 10:06:44 +0900

    BJ

---
+++ src/SsangYong/Exam04.java
@@ -0,0 +1,18 @@
+package SsangYong;
+import java.util.Scanner;
+
+public class Exam04 {
+	static final double WIDTH = 3.3;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input;
+		double result;
+		System.out.println("땅의 평수를 입력하시오.");
+		input = sc.nextInt();
+		result = input * WIDTH;
+		System.out.println(input + "평 땅은 " + result +  "평방미터 입니다.");
+		
+
+	}
+
+}
--- src/boostcamp/Back11720.java
+++ src/boostcamp/Back11720.java
@@ -6,14 +6,12 @@ public class Back11720 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int number = sc.nextInt();
-		String result[] = new String[number];
-		int sum = 0;
-		result[number] = sc.next();
-		
-		for (int i =0; i <number; i++) {
-			sum += (int)result[i];
+		char result[] = new char[number];
+		for (int i = 0; i < number; i++) {
+			result[i] = sc.next().charAt(0);
 		}
-
+		
+		System.out.printf("%c", result[0]);
 	}
 
 }
