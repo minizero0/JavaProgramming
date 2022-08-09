9064fd33ea3701623e2fbca7bf9b4867032c3fc8
yeongmin <dudals1069@gmail.com>
2022-08-09 15:41:39 +0900

    0809

--- src/SsangYong220809/GetAvg.java
+++ src/SsangYong220809/GetAvg.java
@@ -6,17 +6,18 @@ public class GetAvg {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String name;
-		int a,b,c;
+		int a,b,c,sum;
 		double avg;
 		System.out.println("이름과 국어, 영어, 수학점수를 입력하시오.");
 		name = sc.next();
 		a = sc.nextInt();
 		b = sc.nextInt();
 		c = sc.nextInt();
-		avg = (a + b + c) / (double)3;
-		
+		sum = (a + b + c);
+		avg = sum / (double)3;
+		System.out.printf("이름: %-10s, 총점: %-10d, 평균 : %.2f\n", name, sum, avg );
 		if (avg >= 60)
-			System.out.println(name + "평균 : " + avg + "합격");
+			System.out.printf(name + "평균 : %.3f" + "합격",avg);
 		else
 			System.out.println(name + "평균 : " + avg + "불합격");
 
--- src/boostcamp/Back1157.java
+++ src/boostcamp/Back1157.java
@@ -6,15 +6,27 @@ public class Back1157 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
-		int arr[] = new int[25];
+		int arr[] = new int[26];
+		
 		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) >= 97)
-				arr[s.charAt(i -= 97)];
+			if (65 <= s.charAt(i) && s.charAt(i) <= 90) {
+				arr[s.charAt(i) - 65]++;
+			}
 			else
-			arr[s.charAt(i) - 9]++;
+				arr[s.charAt(i) - 97]++;
 		}
 		
-
+		int max = -1;
+		char ch = '?';
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+				ch = (char)(i + 65);
+			}
+			else if (max == arr[i])
+				ch = '?';
+		}
+		System.out.println(ch);
 	}
 
 }
