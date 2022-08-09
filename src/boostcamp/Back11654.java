4bb5309f76d456f16c19d9ae6db1e8aff560ae1f
yeongmin <dudals1069@gmail.com>
2022-08-09 09:30:45 +0900

    BJ

--- src/boostcamp/Back11654.java
+++ src/boostcamp/Back11654.java
@@ -1,10 +1,12 @@
 package boostcamp;
+import java.util.Scanner;
 
 public class Back11654 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-
+		char alpha = sc.next().charAt(0);
+		System.out.println((int)alpha);	
 	}
-
 }
