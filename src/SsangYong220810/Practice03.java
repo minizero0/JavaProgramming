d836556ad61b087d0d496ab3b947fe8f3d207277
yeongmin <dudals1069@gmail.com>
2022-08-10 15:36:10 +0900

    0810

--- src/SsangYong220810/Practice03.java
+++ src/SsangYong220810/Practice03.java
@@ -9,16 +9,19 @@ public class Practice03 {
 		System.out.println("몇월 입니까?");
 		wol = sc.nextInt();
 		
-		if (wol >=3 && wol <=5)
-			System.out.println(wol + "월은 봄입니다.");
-		else if(wol >= 6 && wol <=8)
-			System.out.println(wol + "월은 여름입니다.");
-		else if(wol >= 9 && wol <=11)
-			System.out.println(wol + "월은 가을입니다.");
-		else if(wol == 12 || wol == 1 || wol == 2)
-			System.out.println(wol + "월은 겨울입니다.");
+		if (wol >= 1 && wol <= 12) {
+			if (wol >= 3 && wol <= 5)
+				System.out.println(wol + "월은 봄입니다.");
+			else if(wol >= 6 && wol <= 8)
+				System.out.println(wol + "월은 여름입니다.");
+			else if(wol >= 9 && wol <= 11)
+				System.out.println(wol + "월은 가을입니다.");
+			else
+				System.out.println(wol + "월은 겨울입니다.");
+		}
 		else
-			System.out.println("잘못된 입력입니다");
+			System.out.println("입력오류!");
+		
 	}
 
 }
