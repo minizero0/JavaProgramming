7fbbb9fd05bc02179aeaaa5928401e27d6e8812b
yeongmin <dudals1069@gmail.com>
2022-08-11 11:40:31 +0900

    0811

--- src/SsangYong220811/KooCode.java
+++ src/SsangYong220811/KooCode.java
@@ -1,14 +1,14 @@
 package SsangYong220811;
+
 import java.util.Scanner;
-
-public class KooCode {
-
-	public static void main(String[] args) {		
+class	Star  
+{
+	public static void main(String[] args) 
+	{
 		Scanner sc = new Scanner(System.in);
 		String name;
 		String star;
 		int month, day;
-		star = "물병자리"
 		System.out.println("이름을 입력해 주세요");
 		name = sc.next();
 		System.out.println("월을 입력해 주세요");
@@ -25,8 +25,31 @@ public class KooCode {
 			case 1:
 				if(day>=20 && day<=31 )
 			{
-				System.out.println(name+"님의 별자리는" +물병자리+" 입니다..");		
+				star = "물병자리";		
 			}
 				break;
-
+			case 2:
+					if(day>=1 && day<=18 )
+					{
+						star = "물병자리";		
+					}
+					else(day>=19 && day<=28)
+					{
+						star = "물고기자리";
+					}
+				break;
+			case 3:
+					if(day>=1 && day<=20 )
+					{
+						star = "물고기자리";		
+					}
+					else(day>=21 && day<=31)
+					{
+						star = "양자리";
+					}
+				break;
+			default:return;
+		}
+		System.out.println(name+"님의 별자리는"+star+"입니다..");
+	}
 }
--- src/SsangYong220811/StarArea02.java
+++ src/SsangYong220811/StarArea02.java
@@ -2,7 +2,7 @@ package SsangYong220811;
 
 import java.util.Scanner;
 
-public class StarAreaTest {
+public class StarArea02 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -15,18 +15,26 @@ public class StarAreaTest {
 		name = sc.next();
 		System.out.println("몇월에 태어났나요.");
 		month = sc.nextInt();
-		System.out.println("몇일에 태어났나요.");
-		day = sc.nextInt();
 		
 		if (month < 1 || month > 12) {
 			System.out.println("입력오류.");
 			return;
 		}
 		
-		if (day < 1 || day > 31) {
-			System.out.println("입력오류.");
+		int last = 31;
+		switch(month) {
+			case 4:case 6: case 9: case 11:last = 30;break;
+			case 2:last = 28;
+		}
+		
+		System.out.println("몇일에 태어났나요.");
+		day = sc.nextInt();
+		
+		if (day > last) {
+			System.out.println("입력된 날짜가 범위를 넘었습니다.");
 			return;
 		}
+		
 		switch (month) {
 		case 1:if (day <= 19) star = "염소자리";else star = "물병자리";break;
 		case 2:if (day <= 18) star = "물병자리";else star = "물고기자리";break;	
