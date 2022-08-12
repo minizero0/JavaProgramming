6653319c7168cc4e7ed3e476bb8e42b4b6f1b690
yeongmin <dudals1069@gmail.com>
2022-08-12 10:42:16 +0900

    0811

--- src/SsangYong220812/NestedForTest04.java
+++ src/SsangYong220812/NestedForTest04.java
@@ -5,11 +5,14 @@ public class NestedForTest04 { //약수출력 프로그램
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		int cnt = 0;
 		int n = sc.nextInt();
 		for (int i = 1; i <= n; i++) {
-			if (n % i == 0)
+			if (n % i == 0) {
 				System.out.print(i + " ");
+				cnt++;
+			}
 		}
+		System.out.println("\n" + n + "의 약수의 개수 = " + cnt);
 	}
 }
