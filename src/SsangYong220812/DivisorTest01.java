520bb474ff307135cea2a377858a97add445992a
yeongmin <dudals1069@gmail.com>
2022-08-12 11:44:42 +0900

    0811

--- src/SsangYong220812/DivisorTest01.java
+++ src/SsangYong220812/DivisorTest01.java
@@ -4,12 +4,15 @@ public class DivisorTest01 {
 
 	public static void main(String[] args) {
 		for (int i = 1; i <= 100; i++) {
+			int sum = 0;
 			System.out.print(i + "의 약수 : ");
 			for (int j = 1; j <= i; j++) {
-				if (i % j == 0)
+				if (i % j == 0) {
 					System.out.print(j + " ");
+					sum += j;
+				}
 			}
-			System.out.println();
+			System.out.println("==> " + sum);
 		}
 
 	}
