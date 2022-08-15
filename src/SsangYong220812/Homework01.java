b7e15b8eb9625a77778697140a89f781c5146900
yeongmin <dudals1069@gmail.com>
2022-08-15 17:25:27 +0900

    0812

---
+++ src/SsangYong220812/Homework01.java
@@ -0,0 +1,16 @@
+package SsangYong220812;
+
+public class Homework01 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while (i < 101) {
+			i += 3;
+			if (i > 100)
+				break;
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
