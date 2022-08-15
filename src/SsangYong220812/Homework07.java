0b4758c5a61b3febe7304eadbcd33308a17eddcc
yeongmin <dudals1069@gmail.com>
2022-08-15 18:31:01 +0900

    0815

--- src/SsangYong220812/Homework07.java
+++ src/SsangYong220812/Homework07.java
@@ -12,7 +12,5 @@ public class Homework07 {
 		
 		for (int i = arr.length - 1; i >= 0; i--)
 			System.out.println(arr[i]);
-
 	}
-
 }
--- src/SsangYong220812/Homework08.java
+++ src/SsangYong220812/Homework08.java
@@ -1,9 +1,32 @@
 package SsangYong220812;
 
+import java.util.Scanner;
+
 public class Homework08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		double pay[] = new double[12];
+		double sum = 0;
+		int max = 0,min = 1000000;
+		
+		for (int i = 0; i < pay.length ; i++) {
+			pay[i] = sc.nextDouble();
+			if (pay[i] < 0) {
+				System.out.println("잘못된 입력입니다.");
+				return;
+			}
+			sum += pay[i];
+			if (pay[i] > max)
+				max = i;
+			if (pay[i] < min)
+				min = i;
+		}
+		System.out.println("1년 전체 사용금액 : " + sum);
+		System.out.println("월별평균사용금액 : " + sum/12);
+		System.out.println("가장 지출이 많았던 월." + (max+1));
+		System.out.println("가장 지출이 적었던 월." + (min+1));
 
 	}
 
