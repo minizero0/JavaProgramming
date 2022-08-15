6292839d023ce48f4538ad383c138c5d365046cf
yeongmin <dudals1069@gmail.com>
2022-08-15 17:55:50 +0900

    0812

---
+++ src/SsangYong220812/Homework05.java
@@ -0,0 +1,17 @@
+package SsangYong220812;
+
+public class Homework05 {
+
+	public static void main(String[] args) {
+		int studentNumbers[] = new int[30];
+		double profits[] = new double[100];
+		char proverbs[] = new char[2000];
+		double arr[] = {1.1, 3.3, 6.7};
+		
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+				
+
+	}
+
+}
