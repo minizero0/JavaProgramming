98a60e9cc9dcfdafc82d50ade1ef02fa8e7ede6d
yeongmin <dudals1069@gmail.com>
2022-08-16 15:35:17 +0900

    0816

---
+++ src/SsangYong220816/MethodTest04.java
@@ -0,0 +1,16 @@
+package SsangYong220816;
+
+public class MethodTest04 {
+	
+	//매개변수로 두개의 정수를 전달받아 더하기 결과를 출력.
+	static void Plus(int a, int b) {
+		System.out.println(a+b); 
+	}
+
+	public static void main(String[] args) {
+		Plus(1,2);
+		Plus(100,200);
+
+	}
+
+}
