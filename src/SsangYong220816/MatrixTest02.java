e68794996981f7eb1492cd70aee2e0ae911afa90
yeongmin <dudals1069@gmail.com>
2022-08-16 12:51:13 +0900

    0816

--- src/SsangYong220816/MatrixTest02.java
+++ src/SsangYong220816/MatrixTest02.java
@@ -6,22 +6,16 @@ public class MatrixTest02 {
 		int a[][] = new int[4][4];
 		int n = 1;
 		for (int i = 0; i < a.length; i++) {
-			int sum = 0;
-			for (int j = 0; j < a[i].length; j++) {
-				if (a[i].length-1 == j)
-					a[i][j] = sum;
-				else {
-					a[i][j] = n;
-					sum += n++;
-				}
+			for (int j = 0; j < a[i].length -1 ; j++) {
+				a[i][j] = n++;
+				a[i][3] += a[i][j];
 			}
 		}
+		
 		for (int i = 0; i < a.length; i++) {
 			for (int j = 0; j < a[i].length; j++)
 				System.out.printf("%d ",a[i][j]);
 			System.out.println();
 		}
-
 	}
-
 }
