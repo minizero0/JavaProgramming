6626dfb9bc7a7202d5d5453dd1944662e8ad2c8f
yeongmin <dudals1069@gmail.com>
2022-08-16 11:14:10 +0900

    0816

--- src/SsangYong220812/Homework08.java
+++ src/SsangYong220812/Homework08.java
@@ -9,7 +9,6 @@ public class Homework08 {
 		
 		double pay[] = new double[12];
 		double sum = 0;
-		int max = 0,min = 1000000;
 		
 		for (int i = 0; i < pay.length ; i++) {
 			do {
@@ -17,11 +16,17 @@ public class Homework08 {
 				pay[i] = sc.nextDouble();
 			}while(pay[i] < 0);
 			sum += pay[i];
+		}
+		int max = 0;
+		int min = 0;
+		for (int i = 0; i < pay.length; i++) {
+			System.out.println((i+1) + "월 카드 사용금액:");
+			System.out.println(pay[i]);
 			if (pay[i] > max)
 				max = i;
 			if (pay[i] < min)
 				min = i;
-		}
+	}
 		System.out.println("1년 전체 사용금액 : " + sum);
 		System.out.println("월별평균사용금액 : " + sum/12);
 		System.out.println("가장 지출이 많았던 월." + (max+1));
