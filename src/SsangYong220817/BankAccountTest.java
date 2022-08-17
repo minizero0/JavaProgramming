890706550cb8838553c806d38ddefe21b4dfc83b
yeongmin <dudals1069@gmail.com>
2022-08-17 16:22:14 +0900

    0817

--- src/SsangYong220817/BankAccountTest.java
+++ src/SsangYong220817/BankAccountTest.java
@@ -1,23 +1,27 @@
 package SsangYong220817;
 
 class BankAccount{        //은행계좌.
-	private int balance;  //잔액표시 멤버 변수. 
+	private double balance;  //잔액표시 멤버 변수. 
 	int x; //멤버 변수는 자동으로 0으로 초기화됨. boolean : false, String : null, int : 0, double 0.0
 	
-	public void deposit(int amount) {  //저금 
+	public void deposit(double amount) {  //저금 
 		balance += amount;
 	}
-	public void withdraw(int amount) { //인출 
+	public void withdraw(int amount) { //인출
+		if (balance < amount) 
+			System.out.println("금액이 부족합니다.");
+		else
 		balance -= amount;
 	}
-//	public void setBalance(int balance) {
-//		this.balance = balance;
-//	}
-	public int getBalance() {
+	public double getBalance() {
 		return balance;
 	}
 	public void printBalance() {
 		System.out.println(balance);
+	}
+	public void addInterest() {
+		balance *= 1.075;
+		//balance = blanace + (balance * 0.075);
 	}
 }
 
@@ -32,6 +36,9 @@ public class BankAccountTest {
 		
 		a1.withdraw(60);
 		a2.withdraw(30);
+	
+		a1.printBalance();
+		a1.addInterest();
 		a1.printBalance();
 		a2.printBalance();
 		System.out.println(a1.x);
