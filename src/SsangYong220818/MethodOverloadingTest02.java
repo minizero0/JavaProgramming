a180e10e0432baeb618be2e1d7e40a94ad29c640
yeongmin <dudals1069@gmail.com>
2022-08-18 14:45:00 +0900

    0818

--- src/SsangYong220818/MethodOverloadingTest02.java
+++ src/SsangYong220818/MethodOverloadingTest02.java
@@ -11,9 +11,19 @@ class MyUtil1{
 	}
 
 
-	//두개의 정수를 매개변수로 전달받아 그 중에 큰 수를 찾아 반환하는 메소드를 클래스 메소드로 정의	
+	//두개의 정수를 매개변수로 전달받아 그 중에 큰 수를 찾아 반환하는 메소드를 중복정의	
 	public static double findMax(double a, double b) { //메소드 오버로딩.
 		return a > b ? a : b;
+	}
+	
+	//정수형 배열 매개변수로 전달받아 그 중에 가장 큰 수 반환하는 메소드 중복정의
+	public static int findMax(int arr[]) { //메소드 오버로딩.
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max)
+				max = arr[i];
+		}
+		return max;
 	}
 }
 
@@ -24,5 +34,7 @@ public class MethodOverloadingTest02 {
 		System.out.println(result);
 		double result2 = MyUtil1.findMax(1.1, 2.2);
 		System.out.println(result2);
+		int arr[] = {10,30,15,23,4};
+		System.out.println(MyUtil1.findMax(arr));
 	}
 }
