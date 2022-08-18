68af6bc7b1bf0802d3ad556b039382a09ed7557c
yeongmin <dudals1069@gmail.com>
2022-08-18 12:23:42 +0900

    0818

--- src/SsangYong220818/PlaneTest.java
+++ src/SsangYong220818/PlaneTest.java
@@ -10,10 +10,20 @@ class Plane{
 		return planes;
 	}
 	
+	public boolean ifNumber(int number) {
+		if (number > 500) {
+			System.out.println("승객수가 잘못 입력되었습니다.");
+			return false;
+		}else
+			return true;
+	}
+	
 	public Plane() {
 	}
 	
 	public Plane(String company, String model, int number) {
+		if (ifNumber(number) == false)
+			return;
 		this.company = company;
 		this.model = model;
 		this.number = number;
@@ -29,6 +39,12 @@ class Plane{
 		this.company = company;
 		this.model = model;
 		number = 500;
+		planes++;
+	}
+	public Plane(String company, int number) {
+		this.company = company;
+		this.number = number;
+		model = "Debian";
 		planes++;
 	}
 	public Plane (int number) {
@@ -68,15 +84,17 @@ class Plane{
 public class PlaneTest {
 
 	public static void main(String[] args) {
-		Plane p1 = new Plane("Asiana", "A230", 365);
+		Plane p1 = new Plane("Asiana", "A230", 665);
 		Plane p2 = new Plane("KoreaAir", "A230");
 		Plane p3 = new Plane(365);
 		Plane p4 = new Plane();
+		Plane p5 = new Plane("NewYork", 400);
 		
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p3);
 		System.out.println(p4);
+		System.out.println(p5);
 		
 		System.out.println("생성된 비행기 : " + Plane.getPlanes());
 
