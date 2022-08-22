04589edba4d6eebc642b5fddc7dbc5abadf91c9e
yeongmin <dudals1069@gmail.com>
2022-08-22 16:54:45 +0900

    0822

--- src/SsangYong220822/JFrameTest02.java
+++ src/SsangYong220822/JFrameTest02.java
@@ -1,23 +1,38 @@
 package SsangYong220822;
+import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.HeadlessException;
+import java.awt.event.*;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-class MyFrame1 extends JFrame{
+
+class MyEvent implements ActionListener{
+
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("눌러짐");
+	}
+}
+
+class MyFrame1 extends JFrame {
 	public MyFrame1() {
 		super("계산기");
+		JButton btn_sum = new JButton("1~10까지 더하기");
+		JButton btn_sum1 = new JButton("Test");
+		add(btn_sum, BorderLayout.NORTH);
+		add(btn_sum1, BorderLayout.SOUTH);
+		MyEvent me = new MyEvent();
+		btn_sum.addActionListener(me);
 		setSize(300,200);
 		setVisible(true);
+
 	}
+	
 }
 
 public class JFrameTest02 {
 
 	public static void main(String[] args) {
 		MyFrame1 f = new MyFrame1();
-		JButton btn_sum = new JButton("1~10까지 더하기");
-		f.add(btn_sum);
 	}
 }
 
