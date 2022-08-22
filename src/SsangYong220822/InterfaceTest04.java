6c489157e71b5a0ddb842c41df38fbc11701f8dc
yeongmin <dudals1069@gmail.com>
2022-08-22 11:52:16 +0900

    0822

---
+++ src/SsangYong220822/InterfaceTest04.java
@@ -0,0 +1,44 @@
+package SsangYong220822;
+
+class O{
+	int k = 100;
+	public void test() {
+		System.out.println("test");
+	}
+}
+interface Q{
+	int YEAR = 2022; //DEFAULT final . -> final int YEAR
+	public void hello();
+}
+
+interface R{
+	String title = "JAVA"; //DEFAULT final String title
+	public void pro();
+}
+
+class S extends O implements Q,R{
+	String name = "honggild";
+	
+	public void sayHello() {
+		System.out.println("ehoh~ " + name);
+	}
+	public void hello() {
+		System.out.println("hello");
+	}
+	public void pro() {
+		System.out.println("pro");
+	}
+	
+}
+
+public class InterfaceTest04 {
+
+	public static void main(String[] args) {
+		S ob = new S();
+		ob.sayHello();
+		ob.pro();
+		ob.hello();
+
+	}
+
+}
