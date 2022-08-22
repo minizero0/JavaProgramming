94a61efa914f24f9ebd87babc16328f99ad29c3e
yeongmin <dudals1069@gmail.com>
2022-08-22 10:38:56 +0900

    0819

---
+++ src/SsangYong220822/MultipleInherianceOfClass.java
@@ -0,0 +1,21 @@
+package SsangYong220822;
+
+class As{
+	
+}
+class Bs{
+	
+}
+
+class Cs extends As, Bs{
+	
+}
+
+public class MultipleInherianceOfClass {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
