9cb45a4dec98f0285d09735e5995bf91e4434515
yeongmin <dudals1069@gmail.com>
2022-08-22 14:50:58 +0900

    0822

--- src/SsangYong220822/VehicleTest.java
+++ src/SsangYong220822/VehicleTest.java
@@ -1,12 +1,16 @@
 package SsangYong220822;
 
-class Vehicle{
+abstract class Vehicle{
+	protected int speed;
 	protected String name;
 	protected String color;
 	
-	public Vehicle(String name, String color) {
+	public abstract double getKillosPerLiter();
+	
+	public Vehicle(String name, String color, int speed) {
 		this.name = name;
 		this.color = color;
+		this.speed = speed;
 	}
 	public Vehicle() {
 	}
@@ -20,26 +24,27 @@ interface Move{
 }
 
 class Carr extends Vehicle implements Move{
-	private int speed;
 	public Carr(String name, String color,int speed) {
-		super(name, color);
-		this.speed = speed;
+		super(name, color, speed);
 	}
 	public Carr() {
 	}
+	public double getKillosPerLiter() {
+		return 2;
+	}
 	public void SpeedUp(int speed) {
 		System.out.println(speed + "만큼 속도 증가.");
-		this.speed += speed;
+		super.speed += speed;
 	}
 	public void SpeedDown(int speed) {
 		System.out.println(speed + "만큼 속도 감소.");
-		this.speed -= speed;
+		super.speed -= speed;
 	}
 	public void printSpeed() {
-		System.out.println("현재속도 : " + speed + "km");
+		System.out.println("현재속도 : " + super.speed + "km");
 	}
 	public void speedStop() {
-		this.speed = 0;
+		super.speed = 0;
 	}
 }
 
