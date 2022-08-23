e1cfb352cb441cc6661809db8146132a5a1a7994
yeongmin <dudals1069@gmail.com>
2022-08-23 12:22:51 +0900

    0823

--- src/SsangYong220823/DateTest.java
+++ src/SsangYong220823/DateTest.java
@@ -7,7 +7,7 @@ public class DateTest {
 	public static void main(String[] args) {
 		Date today = new Date();
 		int year = today.getYear() + 1900;
-		int month = today.getMonth();
+		int month = today.getMonth() + 1;  //getMonth 는 0~11 사이로 설정되어있음
 		int date = today.getDate();
 		int hours = today.getHours();
 		int minutes = today.getMinutes();
@@ -18,6 +18,11 @@ public class DateTest {
 		System.out.println(hours + "시");
 		System.out.println(minutes + "분");
 		System.out.println(seconds + "초");
+		
+		int day = today.getDay();
+		System.out.println(day);
+		String []arr = {"일", "월", "화", "수", "목", "금", "토"};
+		System.out.println(arr[day] + "요일");
 
 	}
 
---
+++ src/SsangYong220823/DateTest02.java
@@ -0,0 +1,19 @@
+package SsangYong220823;
+
+import java.util.Date;
+
+public class DateTest02 {
+
+	public static void main(String[] args) {
+		Date myDate = new Date();
+		Date startDay = new Date(myDate.getYear(), myDate.getMonth(), 1);
+		int day = startDay.getDay();
+		String []arr = {"일", "월", "화", "수", "목", "금", "토"};
+		System.out.println(arr[startDay.getDay()] + "요일");
+		System.out.println(startDay.getDay());
+		
+		
+
+	}
+
+}
