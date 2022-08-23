6df7d7c31e7f53667ad3b26b9246907e2df8bae4
yeongmin <dudals1069@gmail.com>
2022-08-23 09:46:24 +0900

    0823

---
+++ src/SsangYong220823/MouseEventTest.java
@@ -0,0 +1,20 @@
+package SsangYong220823;
+
+import javax.swing.JFrame;
+
+class MyFrame extends JFrame {
+	public MyFrame() {
+		setTitle("마우스이벤트");
+		setSize(400,300);
+		setVisible(true);
+	}
+}
+
+public class MouseEventTest {
+
+	public static void main(String[] args) {
+		MyFrame m = new MyFrame();
+
+	}
+
+}
