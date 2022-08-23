ef6fd035dbc80599ada95f0ddd90fa0a029eae35
yeongmin <dudals1069@gmail.com>
2022-08-23 23:37:07 +0900

    0823

---
+++ src/SsangYong220823/Hoemwork01.java
@@ -0,0 +1,26 @@
+package SsangYong220823;
+
+public class Hoemwork01 {
+	
+	public static void swap(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - (i+1); j++) {
+//				if (j + 1 == arr.length)
+//					break;
+				if (arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {10,20,5,2,3};
+		swap(arr);
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+	}
+
+}
