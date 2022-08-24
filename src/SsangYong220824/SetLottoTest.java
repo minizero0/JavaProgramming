51067d753521a47eab553d30eaef03b2c4dcfe73
yeongmin <dudals1069@gmail.com>
2022-08-24 10:19:46 +0900

    0824

--- src/SsangYong220824/SetLottoTest.java
+++ src/SsangYong220824/SetLottoTest.java
@@ -1,14 +1,14 @@
 package SsangYong220824;
 
+import java.util.LinkedHashSet;
 import java.util.Random;
-import java.util.TreeSet;
 
 //Set을 이용해 Lotto 생성기 만들기 
 
 public class SetLottoTest {
 
 	public static void main(String[] args) {
-		TreeSet set = new TreeSet();
+		LinkedHashSet set = new LinkedHashSet();
 		Random r = new Random();
 		
 		for (int i = 0; i < 6; i++) {
