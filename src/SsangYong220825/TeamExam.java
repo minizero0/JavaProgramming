f032132813d4ef3541703100d101baf89d183ba1
yeongmin <dudals1069@gmail.com>
2022-08-25 17:20:42 +0900

    0825

--- src/SsangYong220825/TeamExam.java
+++ src/SsangYong220825/TeamExam.java
@@ -1,13 +1,21 @@
 package SsangYong220825;
 //주민번 3,4,5,6,7,8 을 적하여 성별과 나이 판별 출력 .
 import java.util.Scanner;
-
 public class TeamExam {
 	
-	public static int getAge(int n) {
-		return 122 - n;
+	public static void getAge(int n) {
+		System.out.println("age : " + (122 - n));
 	}
-
+	
+	public static void getSex(char ch) {
+		if(ch == '1' || ch == '3' || ch == '5' || ch == '7')
+			System.out.println("sex : male");
+		else if(ch == '2' || ch == '4' || ch == '6'|| ch == '8')
+			System.out.println("set : female");
+		else
+			System.out.println("input error");
+		
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -18,16 +26,10 @@ public class TeamExam {
 			System.out.println("올바르지 않은 번호입니다. 다시 입력하세요.");
 			return;
 		}
-		
-		String s = num.substring(num.indexOf("-"), 8);
+		String s1 = num.substring(num.indexOf("-"), 8);
 		String s2 = num.substring(0, 2); 
 		
-		int age = getAge(Integer.parseInt(s2));
-		if(s.charAt(1) == '1' || s.charAt(1) == '3' || s.charAt(1) == '5' || s.charAt(1) == '7')
-			System.out.println("male");
-		else if(s.charAt(1) == '2' || s.charAt(1) == '4' || s.charAt(1) == '6'|| s.charAt(1) == '8')
-			System.out.println("female");
-		System.out.println("age : " + age);
+		getSex(s1.charAt(1));
+		getAge(Integer.parseInt(s2));
 	}
-
 }
