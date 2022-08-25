7d45a28bac054b90293749f4f80b292505dd6637
yeongmin <dudals1069@gmail.com>
2022-08-25 16:17:19 +0900

    0825

--- src/SsangYong220825/ValueOfTest.java
+++ src/SsangYong220825/ValueOfTest.java
@@ -2,8 +2,27 @@ package SsangYong220825;
 
 public class ValueOfTest {
 
+	//ValueofTest ==> String.ValueOf(Type variable) String 타입으로 변환시켜준다. 
+	public static void pro(String data) {
+		System.out.println("처리된 데이터: " + data);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean isFlag = true;
+		int age = 20;
+		double height = 182.9;
+		char ch = 'K';
+		
+		pro(String.valueOf(isFlag));
+		pro(String.valueOf(age));
+		pro(String.valueOf(height));
+		pro(String.valueOf(ch));
+		
+		pro(isFlag + "");  // 이렇게 간단한 방법으로도 String 으로 변환가능.
+		pro(age + "");
+		pro(height + "");
+		pro(ch + "");
+		
+		
 
 	}
 
