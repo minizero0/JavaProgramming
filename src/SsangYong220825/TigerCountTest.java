8f7c3169a14274874a5aaf59a1c202360bec2313
yeongmin <dudals1069@gmail.com>
2022-08-25 16:11:06 +0900

    0825

--- src/SsangYong220825/TigerCountTest.java
+++ src/SsangYong220825/TigerCountTest.java
@@ -11,5 +11,5 @@ public class TigerCountTest {
 		}
 		System.out.println(cnt);
 	}
-
+	
 }
---
+++ src/SsangYong220825/ValueOfTest.java
@@ -0,0 +1,10 @@
+package SsangYong220825;
+
+public class ValueOfTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
