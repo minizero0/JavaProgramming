59750319102362d39e974b9c02b272d449bcc86d
yeongmin <dudals1069@gmail.com>
2022-08-26 10:16:34 +0900

    0826

--- src/SsangYong220826/CancerTest.java
+++ src/SsangYong220826/CancerTest.java
@@ -15,14 +15,30 @@ public class CancerTest {
 		code = sc.next();
 		
 		int gender = code.charAt(7)-'0';
-		int age = Integer.parseInt(code.substring(0, 2));
+		int year = Integer.parseInt(code.substring(0, 2));
 		switch(gender) {
-			case 1:System.out.println("male");age += 1900;break;
-			case 2:System.out.println("femal");age += 1900;break;
-			case 3:System.out.println("male");age += 2000;break;
-			case 4:System.out.println("female");age += 2000;break;
+			case 1:System.out.println("male");year += 1900;break;
+			case 2:System.out.println("femal");year += 1900;break;
+			case 3:System.out.println("male");year += 2000;break;
+			case 4:System.out.println("female");year += 2000;break;
 		}
-		System.out.println("출생연도 : " + age);
+		System.out.println("출생년도 : " + year);
+		int age = cal.get(cal.YEAR) - year;
+		System.out.println("나이 : " + age);
+	
+		if (age < 40 || cal.get(cal.YEAR) % 2 != year % 2 )
+			System.out.println("암검진 대상자가 아닙니다.");
+		else {
+			String check = "위암,간암";
+			if (age >= 50)
+				check += ",대장암";
+			if (gender == 2 || gender == 4)
+				check += ",유방암,자궁암";
+			System.out.println("무료암건진 대상자입니다.");
+			System.out.println("검진항목은 " + check + "입니다.");
+		}
+		
+		
 
 	}
 
