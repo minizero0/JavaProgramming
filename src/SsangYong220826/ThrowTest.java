04b9683690eeb48345e7972cef8c0a02ff7d368b
yeongmin <dudals1069@gmail.com>
2022-08-26 12:41:20 +0900

    0826

---
+++ src/SsangYong220826/ThrowTest.java
@@ -0,0 +1,11 @@
+package SsangYong220826;
+
+public class ThrowTest {
+
+	public static void main(String[] args) {
+		int r = 4/2;
+		System.out.println("결과 : " + r);
+		throw new ArithmeticException();	//예외 강제 발생.
+	}
+
+}
