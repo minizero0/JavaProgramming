4e3b5391dbd0c61437cae8f3fd5f3f274b66edc3
yeongmin <dudals1069@gmail.com>
2022-08-27 15:01:51 +0900

    0826

--- src/SsangYong220827/Homework01_3.java
+++ src/SsangYong220827/Homework01_3.java
@@ -1,8 +1,40 @@
 package SsangYong220827;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class Gen{
+	private String key;
+	private int value;
+	public Gen(String key, int value) {
+		this.key = key;
+		this.value = value;
+	}
+	public String toString() {
+		return key + " " + value;
+	}
+	public String getKey() {
+		return key;
+	}
+}
+
 public class Homework01_3 {
 
 	public static void main(String[] args) {
+		ArrayList<Gen> ar = new ArrayList<Gen>();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		StringTokenizer st = new StringTokenizer(str);
+		
+		while (st.hasMoreTokens()) {
+			String key = st.nextToken();
+			ar.add(new Gen(key, 1));
+		}
+		
+		Iterator<String> iter = ar.get
+		
 		
 	}
 
--- src/SsangYong220827/Prac.java
+++ src/SsangYong220827/Prac.java
@@ -2,9 +2,16 @@ package SsangYong220827;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Prac {
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		String str1 = sc.nextLine();
+		System.out.println(str);
+		System.out.println(str1);
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		map.put("hello", 1);
 		map.put("java", 4);
