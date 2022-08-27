9b7e00fa14a2aa03e7a7df5e35eafc1cbf0fab25
yeongmin <dudals1069@gmail.com>
2022-08-27 14:26:21 +0900

    0826

--- src/SsangYong220827/Homework01_2.java
+++ src/SsangYong220827/Homework01_2.java
@@ -1,26 +1,32 @@
 package SsangYong220827;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Homework01 {
+public class Homework01_2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		String str = sc.nextLine();
 		StringTokenizer st = new StringTokenizer(str);
-		int n = 1;
 		
 		while (st.hasMoreTokens()) {
 			String key = st.nextToken();
 			if (map.containsKey(key))
-				map.put(key, map.get(key) + 1);
+				map.put(key, map.get(key)+1);
 			else
-				map.put(key, n);			
+				map.put(key, 1);
 		}
-		System.out.println(map);
+		Iterator<String> iter = map.keySet().iterator();
+		
+		while (iter.hasNext()) {
+			String key = iter.next();
+			Integer value = map.get(key);
+			System.out.print(key + " ==> " + value + "\n");
+		}
 	}
 
 }
---
+++ src/SsangYong220827/Prac.java
@@ -0,0 +1,9 @@
+package SsangYong220827;
+
+import java.util.HashMap;
+
+public class Prac {
+	HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+	
+}
