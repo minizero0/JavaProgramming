2c406077ffaa157f4df5a2f947f98771c668cb36
yeongmin <dudals1069@gmail.com>
2022-08-28 23:50:52 +0900

    0826

--- src/SsangYong220827/Homework02.java
+++ src/SsangYong220827/Homework02.java
@@ -1,9 +1,31 @@
 package SsangYong220827;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class Super{
+	String name;
+	int	age;
+	
+	public Super(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
+
 public class Homework02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Super> ar = new ArrayList<Super>();
+		
+		ar.add(new Super("tony", 45));
+		ar.add(new Super("parker", 25));
+		
+		Iterator<Super> iter = ar.iterator();
+		
+		while(iter.hasNext()) {
+			
+		}
 
 	}
 
